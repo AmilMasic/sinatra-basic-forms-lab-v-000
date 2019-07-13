456ceb6646b9e7c46ca08e6ead3f385b7c74bba0
Amil Masic <amil.masic@gmail.com>
2019-07-13 14:16:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,10 +11,10 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
+    puppy = Puppy.new(params[:name], params[:breed], params[:age])
     @name = puppy.name
     @breed = puppy.breed
-    @age = puppy.months_old
+    @age = puppy.age
 
     erb :display_puppy
   end
